d6e9ac81efc81d7ddf41dd45d6a254d72704b3bf
Rao Fahad <raofahad046@gmail.com>
2020-12-07 00:17:26 +0500

    demo code of react useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect, useState } from 'react'
+// useEffect is called after the component is rendered and after that useEffect does his work
 function App() {
+  let data = {title: "waiting for data"};
+  const [ todo, setTodo ] = useState(data);
+  // const [ isFetching, setFetching ] = useState(false);
+  useEffect(() => {
+    async function fetchData(){
+      // setFetching(true);
+      const response = await fetch('http://jsonplaceholder.typicode.com/todos/1');
+      console.log('response = ', response);
+      let data2 = await response.json();
+      setTodo(data2);
+      // setFetching(false);
+      console.log('data = ', todo);
+    }
+    fetchData();
+  }, []);
+  if(isFetching){
+    return <div>Data is loading......</div>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello Fetch
+      <br />
+    <span>title: {todo.title}</span>
     </div>
   );
 }
